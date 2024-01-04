6e553472eaf87aa340688ad676a1daa24efc0eb3
smart-life-tech <christlightworld@gmail.com>
2024-01-04 14:37:01 +0100

    modified:   test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-
+import re
 def control_conveyor(weight1, weight2):
     try:
         if weight1 is not None and weight2 is not None:
@@ -23,6 +23,10 @@ def control_conveyor(weight1, weight2):
     except Exception as e:
         print("Error in conveyor control:", e)
 
-w1= float(input(" enter for weight 1 : "))
-w2=float(input("enter input for weight 2 : "))
+w1= str(input(" enter for weight 1 : "))
+w1=(re.findall(r'-?\d+\.\d+|-?\d+', w1))
+w1=float(w1[0])
+w2=str(input("enter input for weight 2 : "))
+w2=(re.findall(r'-?\d+\.\d+|-?\d+', w2))
+w2=float(w2[0])
 control_conveyor(w1,w2)
