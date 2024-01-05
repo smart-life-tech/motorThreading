5890a103cb5037574912709dad0545c04a35f508
smart-life-tech <christlightworld@gmail.com>
2024-01-05 18:35:01 +0100

    modified:   thread.py

--- thread.py
+++ thread.py
@@ -155,7 +155,7 @@ def control_relay1(weight):
 
 def control_relay2(weight):# all these section is same as using weight 2
     #time.sleep(5)
-    global w1,w2
+    #global w1,w2
     try:
         if weight is not None:
             '''
